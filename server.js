12a041881eb11f99ddf68d794ec8b4ea723d8f41
Marcel Degas <marcel.degas@gmail.com>
2016-05-03 14:09:30 -0700

    added data to the index page using templating

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ app.set('view engine', 'ejs');
 
 //so cool!  res.render will automatically search for a views folder!
 app.get('/', function(req, res) {
-	res.render('pages/index');
+	var drinks = [
+		{ name: 'Bloody Mary', drunkeness: 3}, 
+		{ name: 'Martini', drunkeness: 5}, 
+		{ name: 'Scotch', drunkeness: 10}, 
+
+	];
+
+	var tagline = "Any code of your own you havent' looked at for 6 months might as well have been written by somebody else";
+	
+	res.render('pages/index', {
+		drinks: drinks, 
+		tagline: tagline
+	});
 });
 
 app.get('/about', function(req, res) {
